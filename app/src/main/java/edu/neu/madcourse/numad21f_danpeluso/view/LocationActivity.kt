7a1aa8839f434c95eb07a6a598177bc4de6d3c69
Dan <pelusodan@gmail.com>
2021-02-25 10:32:56 -0500

    adding location listener

--- app/src/main/java/edu/neu/madcourse/numad21f_danpeluso/view/LocationActivity.kt
+++ app/src/main/java/edu/neu/madcourse/numad21f_danpeluso/view/LocationActivity.kt
@@ -3,20 +3,33 @@ package edu.neu.madcourse.numad21f_danpeluso.view
 import android.annotation.SuppressLint
 import android.content.Context
 import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import edu.neu.madcourse.numad21f_danpeluso.R
 
 class LocationActivity : AppCompatActivity() {
 
     private lateinit var locationTextView: TextView
+    private lateinit var locationGrabButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location)
         grabLocation()
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        locationGrabButton = findViewById(R.id.location_grab_button)
+        locationGrabButton.setOnClickListener {
+            locationTextView.text = "Getting location..."
+            grabLocation()
+        }
     }
 
     // we can suppress this as we are only opening the activity if the permission is enabled
@@ -25,7 +38,23 @@ class LocationActivity : AppCompatActivity() {
         locationTextView = findViewById(R.id.location_textview)
         val locationManager = (getSystemService(Context.LOCATION_SERVICE) as LocationManager)
         locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).also { changeLatLongText(it) }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                500L,
+                .5f,
+                object : LocationListener {
+                    override fun onLocationChanged(location: Location?) {
+                        location?.let { changeLatLongText(it) }
+                    }
 
+                    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+                        Toast.makeText(parent, "Status Changed", Toast.LENGTH_LONG).show()
+                    }
+
+                    override fun onProviderEnabled(provider: String?) {}
+
+                    override fun onProviderDisabled(provider: String?) {}
+
+                })
     }
 
     private fun changeLatLongText(location: Location?) {
